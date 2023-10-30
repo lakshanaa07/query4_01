5b47af288015022a9e17b5edf6e9ab455569e51d
lakshanaa07 <148964858+lakshanaa07@users.noreply.github.com>
2023-10-30 15:23:17 +0530

    Update stage5.sql

--- models/example/stage5.sql
+++ models/example/stage5.sql
@@ -1,21 +1,6 @@
 WITH
 data AS(
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    SELECT * FROM `cloudside-academy.dbt_sample_data.annual-enterprise-survey02`),
-=======
-    SELECT * FROM 'cloudside-academy.dbt_sample_data.annual-enterprise-survey01'),
->>>>>>> 7fc43ca819ffa596e482c859bd95b45370315fcf
-
-a AS(
-=======
     SELECT * FROM `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`)a AS(
->>>>>>> 574550f8a1a8500327c51e46ffe157f6aae9f3ed
-=======
-    SELECT * FROM `cloudside-academy.dbt_sample_data.annual-enterprise-survey01`),
-a AS(
->>>>>>> 6907c7a878ec1bd63e42204c92dcc03057598c5e
     SELECT
         id AS table2_id,
         Year AS table2_year,
