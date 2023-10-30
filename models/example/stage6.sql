1bf6e1df31ce87abc8ab00543a23324ec0dc2810
lakshanaa07 <148964858+lakshanaa07@users.noreply.github.com>
2023-10-30 12:34:37 +0530

    Update stage6.sql

--- models/example/stage6.sql
+++ models/example/stage6.sql
@@ -4,16 +4,16 @@ data AS(
 
 b AS(
     SELECT 
-        id, 
-        year, 
-        industry_code_ANZSIC,
-        ndustry_name_ANZSIC,
-        rme_size_grp,
-        variable,
-        value,
-        unit,
-        serial_number,
-        serial_number_1,
+        id AS table1_id, 
+        year AS table1_year, 
+        industry_code_ANZSIC AS table1_industry_code,
+        industry_name_ANZSIC AS table1_industry_name,
+        rme_size_grp AS table1_rme_size_grp,
+        variable AS table1_variable,
+        value AS table1_value,
+        unit AS table1_unit,
+        serial_number AS table1_serial_number,
+        serial_number_1 AS table1_serial_number_1
     FROM data
 )
 
