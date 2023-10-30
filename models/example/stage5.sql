0c14ff1b5d7661d63cb9348715e2342b17159637
lakshanaa07 <148964858+lakshanaa07@users.noreply.github.com>
2023-10-30 12:05:28 +0530

    Update stage5.sql

--- models/example/stage5.sql
+++ models/example/stage5.sql
@@ -1,6 +1,6 @@
 WITH
 data AS(
-    SELECT * FROM 'cloudside-academy.dbt_sample_data.annual-enterprise-survey02'),
+    SELECT * FROM 'cloudside-academy.dbt_sample_data.annual-enterprise-survey01'),
 
 a AS(
     SELECT
